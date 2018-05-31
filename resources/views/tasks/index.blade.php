2760bd235f1d81d0fda75f6b4e899f14e6a2ce93
yui.honda <yui.a.honda@rakuten.com>
2018-05-31 05:48:50 +0000

    new massage

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,10 +8,10 @@
         <ul>
             @foreach ($tasks as $task)
              <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
-                {!! link_to_route('messages.create', '新規メッセージの投稿') !!}
                 
             @endforeach
         </ul>
     @endif
-
+     {!! link_to_route('tasks.create', '新規メッセージの投稿') !!}
+                
 @endsection
