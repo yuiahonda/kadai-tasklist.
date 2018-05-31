0d9eb5221964741a791e2e9099b940ff0fdf4f77
yui.honda <yui.a.honda@rakuten.com>
2018-05-31 05:35:58 +0000

    new page

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -8,6 +8,8 @@
         <ul>
             @foreach ($tasks as $task)
              <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+                {!! link_to_route('messages.create', '新規メッセージの投稿') !!}
+                
             @endforeach
         </ul>
     @endif
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -9,7 +9,7 @@
 
     {!! link_to_route('tasks.edit', 'このメッセージを編集', ['id' => $task->id]) !!}
 
-{!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('削除') !!}
     {!! Form::close() !!}
 
